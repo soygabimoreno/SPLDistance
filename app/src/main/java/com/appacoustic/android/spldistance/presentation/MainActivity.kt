563d2d06879c74fa794c8ae64ef0c97f76eec26e
Gabi Moreno <gabrielmorenoibarra@gmail.com>
2020-10-30 04:39:21 +0100

    Portrait

--- app/src/main/java/com/appacoustic/android/spldistance/presentation/MainActivity.kt
+++ app/src/main/java/com/appacoustic/android/spldistance/presentation/MainActivity.kt
@@ -37,6 +37,7 @@ class MainActivity : AppCompatActivity() {
         }, 500)
 
         etDistance.setOnTextChangedListener { input ->
+            throw Exception("FOO")
             viewModel.handleDistanceChanged(input)
         }
     }
