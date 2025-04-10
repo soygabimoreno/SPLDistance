a91bdcd7a2cdbe8f9a250e13ba185e83c4cdce2f
Raúl Luján Colilla <mcdrokz@gmail.com>
2025-04-10 16:50:02 +0200

    Refactor migrate to Gradle Kotlin DSL and update dependencies

--- app/src/main/java/com/appacoustic/android/spldistance/domain/CalculateAttenuation.kt
+++ app/src/main/java/com/appacoustic/android/spldistance/domain/CalculateAttenuation.kt
@@ -1,6 +1,8 @@
 package com.appacoustic.android.spldistance.domain
 
 import arrow.core.Either
+import arrow.core.left
+import arrow.core.right
 import com.appacoustic.android.spldistance.framework.isFilled
 import kotlin.math.log10
 import kotlin.math.round
@@ -13,9 +15,9 @@ class CalculateAttenuation {
         return if (input.isFilled()) {
             val rawResult = 20 * log10(input.toString().toDouble())
             val formattedResult = (round(rawResult * 10) / 10).toFloat()
-            Either.right(formattedResult)
+            formattedResult.right()
         } else {
-            Either.left(InputNotFilledException)
+            InputNotFilledException.left()
         }
     }
 }
